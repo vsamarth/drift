2dafbd9e384d02635349f2fde0d6548f3e8e7661
Samarth Verma <vsamarthj@gmail.com>
2026-04-12 00:16:08 +0530

    feat: add TitleBarShell with draggable area

---
+++ flutter/lib/shell/title_bar_shell.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+class TitleBarShell extends StatelessWidget {
+  const TitleBarShell({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDesktop =
+        Platform.isMacOS || Platform.isWindows || Platform.isLinux;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          if (isDesktop)
+            const DragToMoveArea(
+              child: SizedBox(
+                height: 32,
+                width: double.infinity,
+              ),
+            ),
+          Expanded(child: child),
+        ],
+      ),
+    );
+  }
+}
